13b922da6585bdbb496d085a36839f19d7dc8aec
Cavreek <macogleks@gmail.com>
2023-07-13 12:20:34 +0000

    c programming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point of the program
  *
- * Return: Always returns (0)
+ * Return: Always returns 0
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
