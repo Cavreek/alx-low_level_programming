95a268f5af981c37ba6c5691f3a9faad1b070138
Cavreek <macogleks@gmail.com>
2023-07-14 14:54:40 +0000

    Alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	char alpha_bet;
+
+	for (alpha_bet = 'a'; alpha_bet <= 'z'; alpha_bet++)
+		putchar (alpha_bet);
+	putchar ('\n');
+
+	return (0);
+}
