948f3317f63bcfeffa7d06466c4a978f8576f4f8
Cavreek <macogleks@gmail.com>
2023-07-14 15:33:33 +0000

    printing digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		printf("%d", digit);
+	return (0);
+}
