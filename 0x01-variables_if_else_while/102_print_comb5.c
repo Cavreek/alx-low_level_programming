eb7a0e57b03a56a9e5ce7d5888a98a4a611d3279
Cavreek <macogleks@gmail.com>
2023-07-14 23:13:19 +0000

    combination of digits

---
+++ 0x01-variables_if_else_while/102_print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 0; first < 99; first++)
+	{
+		for (second = first + 1; second < 100; second++)
+		{
+			putchar ((first / 10) + '0');
+			putchar ((first % 10) + '0');
+			putchar (',');
+			putchar ((second / 10) + '0');
+			putchar ((second % 10) + '0');
+			if (first == 98  && second == 99)
+				continue;
+			putchar (',');
+			putchar (' ');
+		}
+	}
+
+	putchar ('\n');
+	return (0);
+}
