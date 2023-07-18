db8e436b1399e23470b86a00483294863801242c
Cavreek <macogleks@gmail.com>
2023-07-18 22:39:25 +0000

    adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * add - Adds two integers
+  * @al: Integer to be added
+  * @bl: Integer to be added
+  *
+  * Return: 0
+  */
+
+int add(int al, int bl)
+{
+	return (al + bl)
+}
