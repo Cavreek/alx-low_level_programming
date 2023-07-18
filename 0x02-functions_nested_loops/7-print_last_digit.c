e69a0d81939430b641b6e12b5013a18b155e1dde
Cavreek <macogleks@gmail.com>
2023-07-18 21:38:30 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,14 +12,15 @@ int print_last_digit(int al)
 	int lt;
 
 	lt = al % 10;
+
 	if (lt < 0)
 	{
 		_putchar(-lt + 48);
-		return (-1);
+		return (-lt);
 	}
 	else
 	{
 		_putchar(lt + 48);
-		return (1);
+		return (lt);
 	}
 }
