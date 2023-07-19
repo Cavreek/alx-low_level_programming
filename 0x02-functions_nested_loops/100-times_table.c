b1a9b136918b9ca9722f5b4539ff5a2359447599
Cavreek <macogleks@gmail.com>
2023-07-19 15:50:14 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,14 +30,19 @@ void print_times_table(int n)
 				}
 				else if (mul <= 99)
 				{
+					_putchar(32);
+					_putchar((mul / 10) + 48);
+					_putchar((mul % 10) + 48);
+				}
+				else
+				{
+					_putchar(((mul / 100) % 10) + 48);
 					_putchar(((mul / 10) % 10) + 48);
-					_putchar(((mul / 10) % 10) + 48);
-					_putchar((mul % 10);
+					_putchar((mul % 10) + 48);
 				}
 			}
 			_putchar('\n');
 		}
 	}
 }
-
 
