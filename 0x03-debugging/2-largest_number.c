980ad99119c21e72d80bfdac4f96ec923d46ca5a
Cavreek <macogleks@gmail.com>
2023-07-19 23:13:26 +0000

    fixing largest number c code

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,38 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if ((a >= 0) && (b >= 0) && (c >= 0))
 	{
-		largest = a;
-	}
-	else if (b > a && b > c)
-	{
-		largest = b;
+		if (a > b && a > c)
+			largest = a;
+		else if (b > a && b > c)
+			largest = b;
+		else
+			largest = c;
+		return (largest);
 	}
 	else
 	{
-		largest = c;
-	}
-	return (largest);
+		if (a < 0)
+		{
+			if (b > c)
+				largest = b;
+			else
+				largest = c;
+		}
+		else if (b < 0)
+		{
+			if (a > c)
+				largest = a;
+			else
+				largest = c;
+		}
+		else if (c < 0)
+		{
+			if (a > b)
+				largest = a;
+			else
+				largest = b;
+		}
+		return (largest);
 }
