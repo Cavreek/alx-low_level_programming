7ae509e7c2f30afc3d97db773411714d10c5245e
Cavreek <macogleks@gmail.com>
2023-07-20 15:27:44 +0000

    prints number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints numbers from 0 to 9
+  * Return: 0
+  */
+
+void print_numbers(void)
+{
+	char al;
+
+	for (al >= 48; al <= 57; al++)
+		_putchar(al);
+	_putchar('\n');
+}
