7f503a46c7bf2108bc8b3434ff4748bf5a83fc50
Cavreek <macogleks@gmail.com>
2023-07-20 19:18:30 +0000

    draws a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,9 +14,13 @@ void print_diagonal(int n)
 	if (n > 0)
 	{
 		for (az = 1; az <= n; az++)
-			_putchar(47);
-		_putchar('\n');
-	}
+		{
+			_putchar(32);
+			if (az == n)
+				_putchar(47);
+			_putchar('\n');
+		}
 	else if (n <= 0)
 		_putchar('\n');
+	_putchar('\n');
 }
