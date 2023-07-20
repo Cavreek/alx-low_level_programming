0cab696f4a495be4118144dcaa4cfc9cf4d544ce
Cavreek <macogleks@gmail.com>
2023-07-20 16:00:02 +0000

    prints 10 times the numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints 10 times the numbers from 0 to 14
+  * Return: 0
+  */
+
+void more_numbers(void)
+{
+	int az, bz;
+
+	for (az = 0; az <= 9; az++)
+	{
+		for (bz = 0; bz <= 14; bz++)
+			_putchar(bz);
+		_putchar(az);
+	}
+	_putchar('\n');
+}
