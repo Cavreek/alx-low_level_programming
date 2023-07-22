02420f442f540cfced37b2597e8a197e7d5bb100
Cavreek <macogleks@gmail.com>
2023-07-22 20:23:35 +0000

    prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,8 +5,27 @@
   * Return: 0
   */
 
-int main void
+int main(void)
 {
+	long numb = 612852475143;
+	long div;
+
+	while (div < (numb / 2))
+	{
+		if ((numb % 2) == 0)
+		{
+			numb /= 2;
+			continue;
+		}
+
+		for (div = 3; div < (numb / 2); div += 2)
+		{
+			if ((numb % div) == 0)
+				numb /= div;
+		}
+	}
+
+	printf("%ld\n", numb);
 
 	return (0);
 }
