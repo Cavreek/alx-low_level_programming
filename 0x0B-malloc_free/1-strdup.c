66b0c78112532379d4129889df96bb0b4192e3c0
Cavreek <macogleks@gmail.com>
2023-08-08 14:01:07 +0000

    duplicate of a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -22,7 +22,7 @@ char *_strdup(char *str)
 	for (x = 0; str[x]; x++)
 		len++;
 
-	second_copy = malloc(sizeof(char) * (len + 1));
+	sec = malloc(sizeof(char) * (len + 1));
 
 	if (sec == NULL)
 		return (NULL);
